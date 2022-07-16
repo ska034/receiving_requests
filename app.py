ebde436ae7b1cfd5e28ee6fff5c2df391322789a
Aliaksandr Yerafeyeu <al.ig.erofeev@mail.ru>
2022-07-16 18:04:44 +0300

    Submitting a request and receiving a response on reddit.com

---
+++ app.py
@@ -0,0 +1,18 @@
+import requests, json
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'])
+def index():
+    res = requests.get("https://api.reddit.com/r/python/hot").text
+    json_file = json.loads(res)
+    return f'''
+    Author: {json_file['data']['children'][0]['data']['author']}\n 
+    Title: {json_file['data']['children'][0]['data']['title']}
+'''
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
